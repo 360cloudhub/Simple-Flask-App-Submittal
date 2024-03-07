bffbfee9afc0534beeca99c807b464a87707def7
360cloudhub <78166838+360cloudhub@users.noreply.github.com>
2024-03-06 18:51:14 -0600

    First push

---
+++ README.md
@@ -0,0 +1,29 @@
+# Docker Exercise
+
+Base on the requirment here are the activities performed:
+
+- [Dockerfile](./Dockerfile) available here.
+- [docker-compose.yml](./docker-compose.yml) available here.
+
+In `docker-compose.yml`, you can modify the following lines to add your custom message/greeting.
+
+```
+    environment:
+      - GREETING=the custom greeting
+```
+
+To run docker-compose, run this command:
+
+`docker-compose up --build`
+
+`--build` ensures you are getting your latest code always.
+
+# Scripting Exercise
+
+- [script.py](./script.py) helps in acheiving this whole job, it creates `files` directory in current folder and also verifies the SHA256 match for each `id` and its `name` retrieved from our API.
+
+# Reverse Proxy 
+
+- The proxy section in docker-compose file will run an nginx container to establish reverse proxy for our application.
+
+- Nginx needs the [nginx.conf](./nginx.conf) file. In this file we are routing every request coming to nginx on port `80` to our application `app` (hostname == service name in docker-compose file) at port `5000`.
---
+++ script.py
@@ -0,0 +1,41 @@
+import os
+import requests
+import hashlib
+
+# Get the data from the URL
+import os
+import requests
+import hashlib
+
+SCRAPE_URL = os.environ.get('SCRAPE_URL', 'http://localhost/data')
+
+try:
+    response = requests.get(SCRAPE_URL)
+    data = response.json()
+    samples = data['samples']
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while making the request: {e}")
+    samples = []
+except KeyError:
+    print("The 'samples' key was not found in the response data.")
+    samples = []
+
+
+if not os.path.exists('files'):
+    os.makedirs('files')
+
+for item in samples:
+    id = item['id']
+    name = item['name']
+
+    # Verify the SHA256 sum of the name matches the id
+    sha256 = hashlib.sha256(name.encode()).hexdigest()
+    if sha256 != id:
+        print(f"SHA256 sum of name does not match id for item {id}. Skipping this!")
+        continue
+    else:
+        print(f"SHA256 sum of name matches id for item {id}. Writing to file...")
+
+    # Write the name to a file in the files directory
+    with open(f"files/{id}.txt", 'w') as file:
+        file.write(name)
